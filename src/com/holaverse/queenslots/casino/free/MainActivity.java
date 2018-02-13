f6a8a21ba3e91e5ae283eadc69de561b67cf6acf
wangtao <t.wang@avid.ly>
2018-02-13 17:19:39 +0800

    2102 demo commit

---
+++ src/com/holaverse/queenslots/casino/free/MainActivity.java
@@ -0,0 +1,69 @@
+package com.avidly.ballz;
+
+import static android.Manifest.permission.READ_PHONE_STATE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	private static final String TAG = "AdsSdk_demo";
+
+	Button btnVideo;
+	Button btnBanner;
+	Button btnInterstitial;
+	Button btnExit;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this, WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{WRITE_EXTERNAL_STORAGE, READ_PHONE_STATE}, 001);
+        }
+        
+		btnBanner = (Button) findViewById(R.id.btnBanner);
+		btnBanner.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, BannerActivity.class);
+				startActivity(intent);
+			}
+		});
+
+		btnInterstitial = (Button) findViewById(R.id.btnInterstitial);
+		btnInterstitial.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, InterstitialActivity.class);
+				startActivity(intent);
+			}
+		});
+
+		btnVideo = (Button) findViewById(R.id.btnVideo);
+		btnVideo.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, VideoActivity.class);
+				startActivity(intent);
+			}
+		});
+
+		btnExit = (Button) findViewById(R.id.btnExit);
+		btnExit.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, ExitActivity.class);
+				startActivity(intent);
+			}
+		});
+	}
+}
